12a4b1b9d8269d44ce92a8148f14bd7092b33cf2
shiva <shiva@jaguarsoftwareindia.com>
2023-02-14 17:52:22 +0530

    added right side menu dropdown component

--- src/components/modals/NavBar.vue
+++ src/components/modals/NavBar.vue
@@ -3,10 +3,31 @@
     <q-avatar>
       <img src="src/assets/img/JaguarWhite.png" />
     </q-avatar>
-
     <q-toolbar-title>
-      <p class="text-subtitle1 q-pt-md text-white"> Jaguar 360° Cloud</p>
+      <p class="text-subtitle1 q-pt-md text-white">Jaguar 360° Cloud</p>
     </q-toolbar-title>
+    <q-btn round dense flat color="white" icon="fa-solid fa-envelope">
+      <q-badge color="red" text-color="white" floating> 4 </q-badge>
+      <q-tooltip>email</q-tooltip>
+    </q-btn>
+    <q-btn round dense flat color="white" icon="fa-solid fa-message">
+      <q-badge color="red" text-color="white" floating> 3 </q-badge>
+      <q-tooltip>messages</q-tooltip>
+    </q-btn>
+    <q-btn round dense flat color="white" icon="fa-solid fa-comments">
+      <q-badge color="red" text-color="white" floating> 4 </q-badge>
+      <q-tooltip>sms</q-tooltip>
+    </q-btn>
+    <q-btn round dense flat color="white" icon="fa-solid fa-bell">
+      <q-badge color="red" text-color="white" floating> 2 </q-badge>
+      <q-tooltip>Notifications</q-tooltip>
+    </q-btn>
+    <q-btn flat :ripple="false" class="bg-purple-6">
+      Jaguar software india
+      <RightMenuDropDowns />
+    </q-btn>
   </q-toolbar>
 </template>
-
+<script setup>
+import RightMenuDropDowns from 'src/components/modals/RightMenuDropDowns.vue';
+</script>
---
+++ src/components/modals/RightMenuDropDowns.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-menu fit>
+    <q-list style="min-width: 100px">
+      <q-item clickable>
+        <q-item-section>New tab</q-item-section>
+      </q-item>
+      <q-item clickable>
+        <q-item-section>New incognito tab</q-item-section>
+      </q-item>
+      <q-separator />
+      <q-item clickable>
+        <q-item-section>Recent tabs</q-item-section>
+      </q-item>
+      <q-item clickable>
+        <q-item-section>History</q-item-section>
+      </q-item>
+      <q-item clickable>
+        <q-item-section>Downloads</q-item-section>
+      </q-item>
+      <q-separator />
+      <q-item clickable>
+        <q-item-section>Settings</q-item-section>
+      </q-item>
+      <q-separator />
+      <q-item clickable>
+        <q-item-section>Help &amp; Feedback</q-item-section>
+      </q-item>
+    </q-list>
+  </q-menu>
+</template>
