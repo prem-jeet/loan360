2df98e991fd20c08575c78a23ac4a97528000809
prem-jeet <jeet4300795@gmail.com>
2023-02-19 09:14:15 +0530

    fixed: use router.push to prevent page refresh (window.location.assign causing store refresh)

--- src/composables/moduleSelectorKeyboardListener.ts
+++ src/composables/moduleSelectorKeyboardListener.ts
@@ -1,61 +1,74 @@
 import { onMounted, onUnmounted } from 'vue';
 
-type moduleUrls =
-  | '/los'
-  | '/lms'
-  | '/collection'
-  | '/fa'
-  | '/td'
-  | '/maintenance'
-  | '/setting';
+type moduleUrlKeys =
+  | 'los'
+  | 'lms'
+  | 'collection'
+  | 'fa'
+  | 'td'
+  | 'maintenance'
+  | 'settings';
 
-const navigateToUrl = (url: moduleUrls) => window.location.assign(url);
-
-const callback = ({ key }: { key: string }) => {
-  /* loan origination system */
-  if (key === 'o' || key === 'O') {
-    navigateToUrl('/los');
-    return;
-  }
-
-  /* loan management system */
-  if (key === 'l' || key === 'L') {
-    navigateToUrl('/lms');
-    return;
-  }
-
-  /* collection */
-  if (key === 'c' || key === 'C') {
-    navigateToUrl('/collection');
-    return;
-  }
-
-  /* financial accounting */
-  if (key === 'a' || key === 'a') {
-    navigateToUrl('/fa');
-    return;
-  }
-
-  /* term deposit */
-  if (key === 'd' || key === 'D') {
-    navigateToUrl('/td');
-    return;
-  }
-
-  /* maintenance */
-  if (key === 'm' || key === 'M') {
-    navigateToUrl('/maintenance');
-    return;
-  }
-
-  /* setting */
-  if (key === 's' || key === 's') {
-    navigateToUrl('/setting');
-    return;
-  }
+const moduleUrls = {
+  los: '/module/los',
+  lms: '/module/lms',
+  collection: '/module/collection',
+  fa: '/module/fa',
+  td: '/module/td',
+  maintenance: '/module/maintenance',
+  settings: '/module/settings',
 };
 
-export function useModuleSelectorKeyboardListener() {
+export function useModuleSelectorKeyboardListener(
+  navigationFunction: (to: string) => void
+) {
+  const navigateToUrl = (key: moduleUrlKeys) =>
+    navigationFunction(moduleUrls[key]);
+
+  const callback = ({ key }: { key: string }) => {
+    /* loan origination system */
+    if (key === 'o' || key === 'O') {
+      navigateToUrl('los');
+      return;
+    }
+
+    /* loan management system */
+    if (key === 'l' || key === 'L') {
+      navigateToUrl('lms');
+      return;
+    }
+
+    /* collection */
+    if (key === 'c' || key === 'C') {
+      navigateToUrl('collection');
+      return;
+    }
+
+    /* financial accounting */
+    if (key === 'a' || key === 'a') {
+      navigateToUrl('fa');
+      return;
+    }
+
+    /* term deposit */
+    if (key === 'd' || key === 'D') {
+      navigateToUrl('td');
+      return;
+    }
+
+    /* maintenance */
+    if (key === 'm' || key === 'M') {
+      navigateToUrl('maintenance');
+      return;
+    }
+
+    /* setting */
+    if (key === 's' || key === 's') {
+      navigateToUrl('settings');
+      return;
+    }
+  };
+
   onMounted(() => {
     console.log('Listening for keypress....');
     window.addEventListener('keydown', callback);
