dfd1277da69ab83ed9e4e5213882efcc0a9a9ce5
prem-jeet <jeet4300795@gmail.com>
2023-02-20 08:01:38 +0530

    removed code

--- src/pages/modules/loanOrganizationSystem/LoanOrganizationSystem.vue
+++ src/pages/modules/loanOrganizationSystem/LoanOrganizationSystem.vue
@@ -2,16 +2,6 @@
   <div>loan organization system</div>
 </template>
 
-<script setup>
-import { useMenuStore } from 'stores/menu';
-
-import { storeToRefs } from 'pinia';
-
-const menuStore = useMenuStore();
-
-const { menu } = storeToRefs(menuStore);
-
-console.log(menu.value);
-</script>
+<script setup></script>
 
 <style lang="scss" scoped></style>
