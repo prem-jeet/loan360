34fd182ba8c82335cdd139d37c29d4458f7ae1eb
prem-jeet <jeet4300795@gmail.com>
2023-02-20 07:55:11 +0530

    added: routes for modules

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -11,6 +11,53 @@ const routes: RouteRecordRaw[] = [
     component: () => import('pages/Login.vue'),
   },
 
+  {
+    path: '/module',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      {
+        path: 'los',
+        name: 'loanOrganizationSystem',
+        component: () =>
+          import(
+            'pages/modules/loanOrganizationSystem/LoanOrganizationSystem.vue'
+          ),
+      },
+      {
+        path: 'lms',
+        name: 'loanManagementSystem',
+        component: () =>
+          import('pages/modules/loanManagementSystem/LoanManagementSystem.vue'),
+      },
+      {
+        path: 'collection',
+        name: 'collection',
+        component: () => import('pages/modules/collection/Collection.vue'),
+      },
+      {
+        path: 'fa',
+        name: 'financialAccounting',
+        component: () =>
+          import('pages/modules/financialAccounting/FinancialAccounting.vue'),
+      },
+      {
+        path: 'td',
+        name: 'termDeposit',
+        component: () => import('pages/modules/termDeposit/TermDeposit.vue'),
+      },
+      {
+        path: 'maintenance',
+        name: 'maintenance',
+        component: () => import('pages/modules/maintenance/Maintenance.vue'),
+      },
+      {
+        path: 'settings',
+        name: 'settings',
+        component: () => import('pages/modules/settings/Settings.vue'),
+      },
+    ],
+  },
+
   // Always leave this as last one,
   // but you can also remove it
   {
