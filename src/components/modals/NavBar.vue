12b3cb6d15327ea76093116fbe47f48d7aa07b0f
shiva <shiva@jaguarsoftwareindia.com>
2023-02-22 18:17:15 +0530

    removed icons from navbar

--- src/components/modals/NavBar.vue
+++ src/components/modals/NavBar.vue
@@ -6,27 +6,6 @@
     <q-toolbar-title>
       <p class="text-subtitle1 q-pt-md text-white">Jaguar 360° Cloud</p>
     </q-toolbar-title>
-    <q-btn round dense flat color="white" icon="fa-solid fa-envelope">
-      <q-badge color="red" text-color="white" floating> 4 </q-badge>
-      <q-tooltip>email</q-tooltip>
-    </q-btn>
-    &nbsp;
-    <q-btn round dense flat color="white" icon="fa-solid fa-message">
-      <q-badge color="red" text-color="white" floating> 3 </q-badge>
-      <q-tooltip>messages</q-tooltip>
-    </q-btn>
-    &nbsp;
-    <q-btn round dense flat color="white" icon="fa-solid fa-comments">
-      <q-badge color="red" text-color="white" floating> 4 </q-badge>
-      <q-tooltip>sms</q-tooltip>
-    </q-btn>
-    &nbsp;
-    <q-btn round dense flat color="white" icon="fa-solid fa-bell">
-      <q-badge color="red" text-color="white" floating> 2 </q-badge>
-      <q-tooltip>Notifications</q-tooltip>
-    </q-btn>
-    &nbsp;
-
     <RightMenuDropDown />
   </q-toolbar>
 </template>
