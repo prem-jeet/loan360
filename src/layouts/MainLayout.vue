cd98ad4af7a8a9a7e1819f491a7c3b9ef99899eb
prem-jeet <jeet4300795@gmail.com>
2023-02-23 17:02:46 +0530

    added todo: open menu event for pages

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,11 +5,15 @@
     </q-header>
     <q-page-container>
       <q-page>
-        <RouterView></RouterView>
+        <RouterView @openMenu="openMenu"></RouterView>
       </q-page>
     </q-page-container>
   </q-layout>
 </template>
 <script setup lang="ts">
 import NavBar from 'src/components/modals/NavBar.vue';
+
+const openMenu = () => {
+  // TODO : open menu
+};
 </script>
