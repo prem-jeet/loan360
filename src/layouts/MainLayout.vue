3e1bfa3c516ad721e0349e1b075b2cbb48ba8d5d
prem-jeet <jeet4300795@gmail.com>
2023-03-08 00:26:32 +0530

    added drawer in layout (left menu)

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,19 +1,30 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="hHh Lpr lFf">
     <q-header elevated>
-      <NavBar />
+      <NavBar @openMenu="openMenu" />
     </q-header>
     <q-page-container>
       <q-page>
         <RouterView @openMenu="openMenu"></RouterView>
       </q-page>
     </q-page-container>
+    <q-drawer v-model="drawerLeft" overlay elevated>
+      <LeftMenu :key="menuStore.currentModule" />
+    </q-drawer>
   </q-layout>
 </template>
 <script setup lang="ts">
 import NavBar from 'src/components/NavBar.vue';
+import LeftMenu from 'src/components/LeftMenu.vue';
+import { ref } from 'vue';
+
+import { useMenuStore } from 'src/stores/menu/menuStore';
+
+const drawerLeft = ref(false);
+
+const menuStore = useMenuStore();
 
 const openMenu = () => {
-  // TODO : open menu
+  drawerLeft.value = !drawerLeft.value;
 };
 </script>
