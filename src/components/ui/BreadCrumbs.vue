c51154914e055bfa61c0cb77ef17c98c28c0f4fd
prem-jeet <jeet4300795@gmail.com>
2023-03-09 22:29:23 +0530

    added breadcrumb component

---
+++ src/components/ui/BreadCrumbs.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-breadcrumbs gutter="sm">
+    <template v-slot:separator>
+      <q-icon size="1rem" name="fa-solid fa-location-arrow" color="primary" />
+    </template>
+
+    <q-breadcrumbs-el to="/moduleSelector" label="Home" icon="home" />
+    <q-breadcrumbs-el
+      v-for="{ path, label } in orderedPaths"
+      :key="label"
+      :to="path"
+      :label="label"
+    />
+  </q-breadcrumbs>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  orderedPaths: { path: string; label: string }[];
+}>();
+</script>
+
+<style scoped></style>
