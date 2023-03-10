b6bb499438e22852d90807f2543e85d0b4680221
prem-jeet <jeet4300795@gmail.com>
2023-03-10 20:56:46 +0530

    added notification utility

---
+++ src/utils/notification.ts
@@ -0,0 +1,39 @@
+import { Notify } from 'quasar';
+
+type Positions =
+  | 'top'
+  | 'top-left'
+  | 'top-right'
+  | 'bottom-left'
+  | 'bottom-right'
+  | 'bottom'
+  | 'left'
+  | 'right'
+  | 'center'
+  | undefined;
+
+export const onSuccess = (data: {
+  msg: string;
+  icon: string;
+  position?: Positions;
+}) => {
+  Notify.create({
+    message: data.msg,
+    color: 'green-8',
+    position: data.position || 'top',
+    icon: data.icon,
+  });
+};
+
+export const onFailure = (data: {
+  msg: string;
+  icon: string;
+  position?: Positions;
+}) => {
+  Notify.create({
+    message: data.msg,
+    color: 'red-8',
+    position: data.position || 'top',
+    icon: data.icon,
+  });
+};
