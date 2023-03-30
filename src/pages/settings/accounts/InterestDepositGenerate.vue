76948aa2f8e0e26c8530d86ab39de4a6860d27e6
prem-jeet <jeet4300795@gmail.com>
2023-03-30 14:39:52 +0530

    added breadcrumb

--- src/pages/settings/accounts/InterestDepositGenerate.vue
+++ src/pages/settings/accounts/InterestDepositGenerate.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>InterestDepositGenerate</div>
+  <div class="absolute q-pa-md full-width full-height bg-gre-4">
+    <q-chip outline square size="md" class="shadow-4" :ripple="false">
+      <BreadCrumbs :ordered-paths="breadcrumbs" />
+    </q-chip>
+    <div class="q-mt-lg q-pb-xl"></div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BreadCrumbs from 'src/components/ui/BreadCrumbs.vue';
+
+const breadcrumbs = [
+  { path: '/module/settings', label: 'Settings' },
+  {
+    path: '/module/settings/interestDepositGenerate',
+    label: 'Deposite Interest Generate',
+  },
+];
+</script>
 
 <style scoped></style>
