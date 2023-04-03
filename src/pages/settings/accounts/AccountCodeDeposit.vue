18f8220376a38891d0faa1a1511d26929c143085
shiva <shiva@jaguarsoftwareindia.com>
2023-04-03 19:36:26 +0530

    loaded accountheads

--- src/pages/settings/accounts/AccountCodeDeposit.vue
+++ src/pages/settings/accounts/AccountCodeDeposit.vue
@@ -7,12 +7,37 @@
 </template>
 
 <script setup lang="ts">
+import { api } from 'src/boot/axios';
 import BreadCrumbs from 'src/components/ui/BreadCrumbs.vue';
+import { ref, onMounted } from 'vue';
 
 const breadcrumbs = [
   { path: '/module/settings', label: 'Settings' },
   { path: '/module/settings/accountcode', label: 'Account Code Deposit' },
 ];
+const fetchingData = ref(false);
+
+interface Accountheads {
+  name: string;
+  accountType: string;
+}
+
+const accountHeads = ref<Accountheads[]>([]);
+onMounted(async () => {
+  fetchingData.value = true;
+  const rsp = await api.get('accountHead');
+
+  if (rsp.data) {
+    accountHeads.value = rsp.data.map(
+      (item: { accountType: string; name: string }) => ({
+        accountType: item.accountType,
+        name: item.name,
+      })
+    );
+    console.log(accountHeads.value);
+  }
+  fetchingData.value = false;
+});
 </script>
 
 <style scoped></style>
