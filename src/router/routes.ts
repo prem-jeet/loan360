8cf5501261bdd661e025f6f45d0a62bb52ce2331
shiva <shiva@jaguarsoftwareindia.com>
2023-04-14 17:19:36 +0530

    all functions added

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -71,6 +71,12 @@ const routes: RouteRecordRaw[] = [
             component: () =>
               import('pages/settings/accounts/InterestDepositGenerate.vue'),
           },
+          {
+            path: 'customerMaster/namePrefix',
+            name: 'namePrefix',
+            component: () =>
+              import('pages/maintenance/customerMaster/NamePrefix.vue'),
+          },
         ],
       },
     ],
