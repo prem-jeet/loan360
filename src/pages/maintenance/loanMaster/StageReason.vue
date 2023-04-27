f44b5b18de173046cccc81dea2f3cc6026e2c805
shiva <shiva@jaguarsoftwareindia.com>
2023-04-27 18:04:51 +0530

    stageReason added all functions

--- src/pages/maintenance/loanMaster/StageReason.vue
+++ src/pages/maintenance/loanMaster/StageReason.vue
@@ -1,9 +1,593 @@
 <template>
-  <div>
-    <h1>Stage Reason</h1>
+  <div class="absolute q-pa-md full-width full-height bg-gre-4">
+    <q-chip outline square size="md" class="shadow-4" :ripple="false">
+      <BreadCrumbs :ordered-paths="breadcrumbs" />
+    </q-chip>
+
+    <div class="row q-mt-lg q-pb-xl">
+      <div class="col">
+        <q-table
+          :rows="filteredData"
+          :columns="columns"
+          row-key="code"
+          :loading="fetchingData"
+          table-header-class="bg-deep-purple-10 text-white"
+          separator="cell"
+          bordered
+          title="Nature entry"
+          :rows-per-page-options="[0]"
+          :hide-bottom="!!filteredData.length"
+          :grid="$q.screen.width < 830"
+          card-container-class="q-gutter-y-md q-mt-xs"
+          :no-data-label="
+            selectedSatge ? 'No result found' : 'Select a segment'
+          "
+        >
+          <template v-slot:top>
+            <div class="row q-gutter-y-lg q-pb-xs-md">
+              <div class="col-12">
+                <div class="row items-center q-gutter-md">
+                  <div class="col-auto text-h6">Stage Reason</div>
+                </div>
+              </div>
+            </div>
+
+            <div class="row full-width q-my-sm">
+              <div class="col-xs-12 col-sm-4 col-md-3 q-pb-sm">
+                <q-select
+                  v-model="selectedSatge"
+                  dense
+                  :options="stages"
+                  map-options
+                  emit-value
+                  label="Select segment"
+                  outlined
+                  hide-bottom-space
+                  :error="selectedError"
+                />
+              </div>
+            </div>
+
+            <div class="row full-width q-mt-sm">
+              <div class="col-xs-12 col-sm-4 col-md-3 q-pb-sm">
+                <q-input
+                  v-model="nameSearchQuery"
+                  outlined
+                  clearable
+                  dense
+                  rounded
+                  placeholder="search"
+                  @clear="nameSearchQuery = ''"
+                >
+                  <template v-slot:prepend>
+                    <q-icon name="search" />
+                  </template>
+                </q-input>
+              </div>
+
+              <div class="col-xs-12 col-sm-3 col-md-6 q-pb-sm">
+                <q-checkbox v-model="checkBox" label=" In-Active" />
+              </div>
+              <div class="col-xs-12 col-sm-5 col-md-3 q-pb-sm">
+                <q-input
+                  v-model="name"
+                  outlined
+                  dense
+                  no-error-icon
+                  :error="error"
+                  :error-message="msg"
+                  placeholder="name"
+                >
+                  <template v-slot:prepend> Reason </template>
+                  <template v-slot:after>
+                    <q-btn
+                      :icon="'add '"
+                      color="teal"
+                      size="md"
+                      :disable="error"
+                      @click="saveEntry()"
+                    />
+                  </template>
+                </q-input>
+              </div>
+            </div>
+          </template>
+
+          <template v-slot:header-cell="props">
+            <q-th :props="props" style="font-size: 1rem">
+              {{ props.col.label }}
+            </q-th>
+          </template>
+
+          <!-- row design for screens > 800px-->
+          <template v-slot:body="props">
+            <q-tr :props="props">
+              <q-td key="actions" auto-width>
+                <q-btn-group push unelevated>
+                  <q-btn
+                    icon="edit"
+                    size="xs"
+                    outline
+                    color="accent"
+                    v-if="editingRowIndex !== props.rowIndex"
+                    @click="() => editEntry(props.row.id, props.rowIndex)"
+                  >
+                    <q-tooltip>Edit</q-tooltip>
+                  </q-btn>
+
+                  <q-btn
+                    v-if="editingRowIndex !== props.rowIndex"
+                    :label="props.row.inactive ? 'activate' : 'deactivate'"
+                    size="xs"
+                    outline
+                    color="red"
+                    @click="changeActive(props.row.id, props.row.inactive)"
+                  >
+                  </q-btn>
+                  <q-btn
+                    icon="check"
+                    size="xs"
+                    outline
+                    color="green-10"
+                    v-if="editingRowIndex === props.rowIndex"
+                    @click="() => saveEdited()"
+                  >
+                    <q-tooltip>Save</q-tooltip>
+                  </q-btn>
+                  <q-btn
+                    icon="close"
+                    size="xs"
+                    outline
+                    color="red"
+                    v-if="editingRowIndex === props.rowIndex"
+                    @click="(isEditing = false), (editingRowIndex = null)"
+                  >
+                    <q-tooltip>Cancel</q-tooltip>
+                  </q-btn>
+                </q-btn-group>
+              </q-td>
+              <q-td key="reason" :props="props">
+                <q-input
+                  v-if="editingRowIndex === props.rowIndex"
+                  v-model="newSouce.reason"
+                  placeholder="Name required"
+                  dense
+                  outlined
+                  :color="newSouce.reason ? 'green' : 'red'"
+                  autofocus
+                />
+                <span v-else>{{
+                  props.row.reason.charAt(0).toUpperCase() +
+                  props.row.reason.slice(1)
+                }}</span>
+              </q-td>
+              <q-td key="createdOn" :props="props">
+                {{
+                  props.row.createdOn.toLocaleString('en-US', DateTimeOptions)
+                }}
+              </q-td>
+              <q-td key="updatedOn" :props="props">
+                {{
+                  props.row.updatedOn.toLocaleString('en-US', DateTimeOptions)
+                }}
+              </q-td>
+              <q-td key="inactiveOn" :props="props">
+                {{
+                  props.row.inactiveOn.toLocaleString('en-US', DateTimeOptions)
+                }}
+              </q-td>
+            </q-tr>
+          </template>
+
+          <!-- card for grid layout screens < 800px -->
+          <template v-slot:item="props">
+            <div class="col-xs-12 col-sm-6 q-px-sm-sm">
+              <q-card>
+                <q-card-section>
+                  <div class="row q-gutter-y-xs">
+                    <div class="col-12 text-weight-medium">Reason :</div>
+                    <div class="col-12">
+                      <q-input
+                        v-if="editingRowIndex === props.rowIndex"
+                        v-model="newSouce.reason"
+                        placeholder="Name required"
+                        dense
+                        outlined
+                        :color="newSouce.reason ? 'green' : 'red'"
+                        autofocus
+                      />
+                      <span v-else>{{
+                        props.row.reason.charAt(0).toUpperCase() +
+                        props.row.reason.slice(1)
+                      }}</span>
+                    </div>
+                  </div>
+                </q-card-section>
+                <q-card-section>
+                  <div class="row q-gutter-y-xs">
+                    <div class="col-12 text-weight-medium">Created :</div>
+                    <div class="col-12">
+                      {{
+                        props.row.createdOn.toLocaleString(
+                          'en-US',
+                          DateTimeOptions
+                        )
+                      }}
+                    </div>
+                  </div>
+                </q-card-section>
+                <q-card-section>
+                  <div class="row q-gutter-y-xs">
+                    <div class="col-12 text-weight-medium">Updated :</div>
+                    <div class="col-12">
+                      {{
+                        props.row.updatedOn.toLocaleString(
+                          'en-US',
+                          DateTimeOptions
+                        )
+                      }}
+                    </div>
+                  </div>
+                </q-card-section>
+                <q-card-section>
+                  <div class="row q-gutter-y-xs">
+                    <div class="col-12 text-weight-medium">Inactive :</div>
+                    <div class="col-12">
+                      {{
+                        props.row.inactiveOn.toLocaleString(
+                          'en-US',
+                          DateTimeOptions
+                        )
+                      }}
+                    </div>
+                  </div>
+                </q-card-section>
+
+                <q-card-actions align="center" class="q-py-md bg-grey-2">
+                  <q-btn
+                    label="edit"
+                    icon="edit"
+                    size="sm"
+                    color="teal"
+                    v-if="editingRowIndex !== props.rowIndex"
+                    @click="() => editEntry(props.row.id, props.rowIndex)"
+                  >
+                    <q-tooltip>Edit</q-tooltip>
+                  </q-btn>
+
+                  <q-btn
+                    v-if="editingRowIndex !== props.rowIndex"
+                    :label="props.row.inactive ? 'activate' : 'deactivate'"
+                    size="sm"
+                    color="red"
+                    @click="changeActive(props.row.id, props.row.inactive)"
+                  >
+                  </q-btn>
+                  <q-btn
+                    label="save"
+                    icon="save"
+                    size="sm"
+                    color="teal"
+                    v-if="editingRowIndex === props.rowIndex"
+                    @click="() => saveEdited()"
+                  >
+                    <q-tooltip>Save</q-tooltip>
+                  </q-btn>
+                  <q-btn
+                    label="close"
+                    icon="close"
+                    size="sm"
+                    color="red"
+                    v-if="editingRowIndex === props.rowIndex"
+                    @click="(isEditing = false), (editingRowIndex = null)"
+                  >
+                    <q-tooltip>Cancel</q-tooltip>
+                  </q-btn>
+                </q-card-actions>
+              </q-card>
+            </div>
+          </template>
+        </q-table>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { api } from 'src/boot/axios';
+import BreadCrumbs from 'src/components/ui/BreadCrumbs.vue';
+import { ref, onMounted, computed, watch, reactive } from 'vue';
+import { onSuccess, confirmDialog, onFailure } from 'src/utils/notification';
+
+interface StageReason {
+  reason: string;
+  stageCode: number | null;
+  id: number | null;
+  createdOn: string;
+  inactive: boolean;
+  inactiveOn: string;
+  updatedOn: string;
+}
+
+interface Stage {
+  label: string;
+  value: string;
+}
+
+const breadcrumbs = [
+  { path: '/module/maintenance', label: 'Maintenance' },
+  {
+    path: '/module/maintenance/loanMaster/stageReason',
+    label: 'Loan Master',
+  },
+  {
+    path: '/module/maintenance/loanMaster/stageReason',
+    label: 'Stage Reason',
+  },
+];
+
+const columns: {
+  name: string;
+  required?: boolean;
+  label: string;
+  field: string;
+  align: 'left';
+}[] = [
+  {
+    name: 'actions',
+    label: 'Actions',
+    align: 'left',
+    field: '',
+  },
+  {
+    name: 'reason',
+    required: true,
+    align: 'left',
+    field: 'reason',
+    label: 'Reason',
+  },
+  {
+    name: 'createdOn',
+    required: true,
+    align: 'left',
+    field: 'createdOn',
+    label: 'Created',
+  },
+  {
+    name: 'updatedOn',
+    required: true,
+    align: 'left',
+    field: 'updatedOn',
+    label: 'Updated',
+  },
+  {
+    name: 'inactiveOn',
+    required: true,
+    align: 'left',
+    field: 'inactiveOn',
+    label: 'In-Active',
+  },
+];
+
+const DateTimeOptions = {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+  hour: 'numeric',
+  minute: 'numeric',
+  hour12: true, // Use 12-hour format
+};
+
+const fetchingData = ref(false);
+const name = ref('');
+const nameSearchQuery = ref('');
+const stages = ref<Stage[]>([]);
+const selectedSatge = ref('');
+const stageReason = ref<StageReason[]>([]);
+const stageReasonTemp = ref<StageReason[]>([]);
+const checkBox = ref(false);
+const isEditing = ref(false);
+const editingRowIndex = ref<number | null>(null);
+const editingRowId = ref<number | null>(null);
+const error = ref(false);
+const msg = ref('');
+const selectedError = ref(false);
+
+const newSouce = reactive<StageReason>({
+  reason: '',
+  stageCode: null,
+  id: null,
+  createdOn: '',
+  inactive: false,
+  inactiveOn: '',
+  updatedOn: '',
+});
+
+const filteredData = computed(() =>
+  stageReason.value.filter((item) => item.inactive === checkBox.value)
+);
+
+const setFormData = () => {
+  const index = stageReason.value.findIndex(
+    (obj) => obj.id === editingRowId.value
+  );
+  newSouce.reason = index >= 0 ? stageReason.value[index].reason : '';
+};
+
+const editEntryConfirmed = (id: number, index: number) => {
+  editingRowIndex.value = index;
+  editingRowId.value = id;
+  setFormData();
+};
+
+const editEntry = (id: number, rowIndex: number) => {
+  if (isEditing.value) {
+    confirmDialog(() => editEntryConfirmed(id, rowIndex), {
+      msg: 'Are you sure you want to cancel editing the current Code?',
+    });
+  } else {
+    isEditing.value = true;
+    editingRowIndex.value = rowIndex;
+    editEntryConfirmed(id, rowIndex);
+  }
+};
+const saveNewEntry = async () => {
+  let payLoad = {
+    reason: name.value,
+    inactive: false,
+    createdOn: new Date(),
+    stageCode: selectedSatge.value,
+  };
+  const rsp = await api.post('/stageReason', payLoad);
+  if (rsp.data) {
+    onSuccess({
+      msg: rsp.data.displayMessage,
+      icon: 'sync_alt',
+    });
+    name.value = '';
+    loadSource();
+  }
+};
+const saveEdited = async () => {
+  const temp = stageReasonTemp.value.filter(
+    (item) => item.id !== editingRowId.value
+  );
+
+  const isDuplicate = temp.find(
+    (item) => item.reason.toLowerCase() === newSouce.reason.toLowerCase()
+  );
+  if (isDuplicate) {
+    onFailure({
+      msg: 'Duplicate Account Found',
+      icon: 'warning',
+    });
+    return;
+  }
+
+  let payLoad = {
+    reason: newSouce.reason,
+    id: editingRowId.value,
+    updatedOn: new Date(),
+    stageCode: selectedSatge.value,
+  };
+  const rsp = await api.put('/stageReason/update', payLoad);
+  if (rsp.data) {
+    onSuccess({
+      msg: rsp.data.displayMessage,
+      icon: 'sync_alt',
+    });
+    isEditing.value = false;
+    editingRowIndex.value = null;
+    loadSource();
+  }
+};
+
+const saveEntry = () => {
+  if (name.value) {
+    selectedSatge.value ? saveNewEntry() : (selectedError.value = true);
+  } else {
+    error.value = true;
+  }
+};
+
+const changeActive = async (id: number, state: boolean) => {
+  if (editingRowIndex.value === null) {
+    confirmDialog(() => changeActiveConfirm(id, state), {
+      msg: state
+        ? 'Are you sure you want to activate ?'
+        : 'Are you sure you want to deactivate ?',
+    });
+  } else {
+    return;
+  }
+};
+
+const changeActiveConfirm = async (id: number, state: boolean) => {
+  const payLoad = {
+    id: id,
+  };
+
+  const str = state ? 'active' : 'inactive';
+  const rsp = await api.put('/stageReason/' + str, payLoad);
+  if (rsp.data) {
+    onSuccess({ msg: rsp.data.displayMessage });
+  }
+  loadSource();
+};
+
+const loadSource = async () => {
+  fetchingData.value = true;
+  const rsp = await api.get('stageReason/stageCode/' + selectedSatge.value);
+
+  if (rsp.data) {
+    stageReason.value = rsp.data.map(
+      (item: {
+        createdOn: string | number | Date;
+        updatedOn: string | number | Date;
+        inactiveOn: string | number | Date;
+      }) => {
+        return {
+          ...item,
+          createdOn: item.createdOn !== null ? new Date(item.createdOn) : '',
+          updatedOn: item.updatedOn !== null ? new Date(item.updatedOn) : '',
+          inactiveOn: item.inactiveOn !== null ? new Date(item.inactiveOn) : '',
+        };
+      }
+    );
+    stageReasonTemp.value = stageReason.value;
+  }
+  fetchingData.value = false;
+};
+
+const loadStages = async () => {
+  fetchingData.value = true;
+  const rsp = await api.get('stage');
+
+  if (rsp.data) {
+    stages.value = rsp.data.map((item: { code: string; name: string }) => {
+      return {
+        label: item.name,
+        value: item.code,
+      };
+    });
+  }
+  fetchingData.value = false;
+};
+
+watch(name, () => {
+  error.value = false;
+  msg.value = '';
+
+  if (!name.value) {
+    return;
+  }
+
+  const temp = stageReasonTemp.value.find(
+    (item) => item.reason.toLowerCase() === name.value.toLocaleLowerCase()
+  );
+
+  if (temp) {
+    error.value = true;
+    msg.value = 'Item already exists!';
+  }
+});
+
+watch(nameSearchQuery, () => {
+  stageReason.value = stageReasonTemp.value.filter((item) => {
+    return item.reason
+      .toLowerCase()
+      .includes(nameSearchQuery.value.toLowerCase());
+  });
+});
+
+watch(selectedSatge, () => {
+  selectedError.value = false;
+  loadSource();
+});
+
+onMounted(() => {
+  loadStages();
+});
+</script>
 
 <style scoped></style>
