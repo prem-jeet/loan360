88b01996272a1810aa432aa272e2eea0bafaf63e
shiva <shiva@jaguarsoftwareindia.com>
2023-05-04 17:09:18 +0530

    added string.js file

---
+++ src/utils/string.ts
@@ -0,0 +1,8 @@
+export const capitalCase = (str: string) =>
+  str
+    .split(' ')
+    .map((s) => s.charAt(0).toUpperCase() + s.slice(1))
+    .join(' ');
+
+export const firstLetterCpitalze = (str: string) =>
+  str.charAt(0).toUpperCase() + str.slice(1);
