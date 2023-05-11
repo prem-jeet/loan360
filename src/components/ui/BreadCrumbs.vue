2c9f9d5ebc9fc2baccc575373f66403d7f1b4303
Bhumi <bhumi.jaguarsoftware@gmail.com>
2023-05-11 23:46:30 +0530

    minor

--- src/components/ui/BreadCrumbs.vue
+++ src/components/ui/BreadCrumbs.vue
@@ -3,6 +3,7 @@
     color="white"
     class="shadow-3 q-py-md-md py-xs-medium q-mx-xs-none"
     :class="customCss || 'q-mx-md-lg ma-sm-none'"
+    :ripple="false"
   >
     <q-breadcrumbs gutter="xs" active-color="indigo" class="q-py-md-xs">
       <template v-slot:separator>
@@ -10,17 +11,12 @@
       </template>
 
       <q-breadcrumbs-el
-        outline
-        rounded
         class="q-px-xs-sm q-py-xs-xs text-subtitle1"
         to="/moduleSelector"
         icon="house"
-        color="gray"
       />
       <q-breadcrumbs-el
         class="q-px-xs-xs q-px-md-sm text-weight-medium q-px-xs-sm q-py-xs-xs"
-        outline
-        rounded
         v-for="{ path, label, disable } in orderedPaths"
         :key="label"
         :to="path"
