cb0ab3d962712a87cfccb86ae3d669a750ec68bc
shiva <shiva@jaguarsoftwareindia.com>
2023-05-11 18:18:12 +0530

    modified code with edit functionality

--- src/components/modals/CommonEditForMaintenancePages.vue
+++ src/components/modals/CommonEditForMaintenancePages.vue
@@ -1,11 +1,16 @@
 <template>
   <q-card style="width: 400px">
-    <q-form>
+    <q-form @submit.prevent="saveEdit" @reset="resetForm">
       <q-card-section class="bg-grey-2">
         <div class="flex items-center">
           <span class="text-subtitle2 q-mr-xl"> Edit Source </span>
           <q-space />
-          <q-btn class="q-ml-xs-md q-ml-sm-xl" icon="close" flat />
+          <q-btn
+            class="q-ml-xs-md q-ml-sm-xl"
+            icon="close"
+            @click="close"
+            flat
+          />
         </div>
       </q-card-section>
       <q-card-section class="q-px-lg q-py-sm">
@@ -20,38 +25,56 @@
             />
           </div>
         </div>
+
         <div class="row justify-between q-mx-sm">
-          <div class="col-4">
+          <div class="col-6">
             <q-btn
               :label="modalObj.inactive ? 'activate' : 'deactivate'"
               size="xs"
-              outline
               color="red"
+              @click="active = true"
             ></q-btn>
           </div>
-          <div class="col-4 text-right text-warning">
+          <div class="col-6 text-right text-warning">
             {{
               modalObj.inactive ? 'Currently Deactive' : 'Currently Activate'
             }}
           </div>
         </div>
-        <div class="row q-mt-md bg-yellow-2">
-          <div class="col-12 q-pl-md q-py-sm">
-            <span> <q-icon name="warning" color="warning" /></span>
-            <span class="text-red-10 q-pl-sm">
-              {{
-                modalObj.inactive
-                  ? 'Are you sure you want to activate ?'
-                  : 'Are you sure you want to deactivate ?'
-              }}
-            </span>
+        <div v-if="active">
+          <div class="row q-mt-md bg-yellow-2">
+            <div class="col-12 q-pl-md q-py-sm">
+              <span> <q-icon name="warning" color="warning" /></span>
+              <span class="text-red-10 q-pl-sm">
+                {{
+                  modalObj.inactive
+                    ? 'Are you sure you want to activate ?'
+                    : 'Are you sure you want to deactivate ?'
+                }}
+              </span>
+            </div>
           </div>
-        </div>
-        <div class="row q-mt-md justify-center text-center">
-          <div class="col-3">
-            <q-btn label="ok" color="teal"></q-btn>
+
+          <div class="row q-mt-md justify-center text-center">
+            <div class="col-3">
+              <q-btn
+                size="sm"
+                label="ok"
+                color="teal"
+                @click="
+                  (modalObj.inactive = !modalObj.inactive), (active = false)
+                "
+              ></q-btn>
+            </div>
+            <div class="col-3">
+              <q-btn
+                size="sm"
+                label="no"
+                color="red-5"
+                @click="active = false"
+              ></q-btn>
+            </div>
           </div>
-          <div class="col-3"><q-btn label="no" color="red-5"></q-btn></div>
         </div>
       </q-card-section>
       <q-separator class="q-mt-md" />
@@ -64,7 +87,7 @@
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { ref, reactive } from 'vue';
 interface EditObject {
   name: string;
   inactive: boolean;
@@ -76,8 +99,23 @@ const props = defineProps({
     required: true,
   },
 });
+const emit = defineEmits(['close', 'saveEdit']);
+let modalObj = reactive<EditObject>({ ...props.editObject });
 
-const modalObj = reactive<EditObject>({ ...props.editObject });
+const active = ref(false);
+
+const close = () => {
+  emit('close');
+};
+
+const resetForm = () => {
+  modalObj.name = props.editObject.name;
+  modalObj.inactive = props.editObject.inactive;
+};
+
+const saveEdit = () => {
+  emit('saveEdit', modalObj);
+};
 </script>
 
 <style scoped></style>
