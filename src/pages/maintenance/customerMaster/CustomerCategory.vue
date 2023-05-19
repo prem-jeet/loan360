7bb3d063aa128476b73e057b91eb67edb9a7a12c
shiva <shiva@jaguarsoftwareindia.com>
2023-05-19 17:33:12 +0530

    modified breadcrumb

--- src/pages/maintenance/customerMaster/CustomerCategory.vue
+++ src/pages/maintenance/customerMaster/CustomerCategory.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="absolute q-pa-md full-width full-height bg-gre-4">
-    <q-chip outline square size="md" class="shadow-4" :ripple="false">
-      <BreadCrumbs :ordered-paths="breadcrumbs" />
-    </q-chip>
+  <div
+    class="absolute q-px-md q-pt-md-sm q-pt-xs-md q-pb-md full-width full-height bg-gre-4"
+  >
+    <BreadCrumbs :ordered-paths="breadcrumbs" :style-css="'q-mx-xs'" />
 
     <div class="row q-mt-lg q-pb-xl">
       <div class="col">
@@ -37,7 +37,7 @@
                   clearable
                   dense
                   rounded
-                  placeholder="search forward"
+                  placeholder="search code"
                   @clear="codeSearchQuery = ''"
                 >
                   <template v-slot:prepend>
@@ -306,6 +306,7 @@ const breadcrumbs = [
   {
     path: '/module/maintenance/customerMaster/customerCategory',
     label: 'Customer Master',
+    disable: true,
   },
   {
     path: '/module/maintenance/customerMaster/customerCategory',
