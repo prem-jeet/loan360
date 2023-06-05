a1c1c2408a5b3146b9417be29bfeef64aee80c5e
Bhumi <bhumi.jaguarsoftware@gmail.com>
2023-06-05 19:05:35 +0530

    minor login page fix

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
+  <div class="center-screen">
     <div class="row">
-      <div class="col">
+      <div class="col gt-xs">
         <div class="justify-center q-px-sm q-mr-md">
-          <q-img :src="loginImage" spinner-color="white" />
+          <q-img :src="loginImage" contain spinner-color="white" />
         </div>
       </div>
       <div class="col-sm justify-center column" align="center">
         <q-card
-          class="q-pa-sm-md q-ma-xs-xs q-mr-sm-xl q-pl-sm-xs q-pb-xs-xs"
-          bordered
+          class="q-pa-sm-md q-ma-xs-xs q-mr-sm-xl q-pl-sm-xs q-pb-xs-md q-pb-sm-none"
+          rounded-borders
         >
           <div class="row inline justify-center">
             <div
