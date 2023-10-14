88b3ac6ac6fd99fce084a5f0b89d3390ca4bffe6
prem-jeet <jeet4300795@gmail.com>
2023-10-14 13:33:38 +0530

    added max height and changed the font size for the add edit form

--- src/components/commonForms/AddEditForm.vue
+++ src/components/commonForms/AddEditForm.vue
@@ -1,22 +1,20 @@
 <template>
   <q-card class="column" :style="{ minWidth: minWidth, minHeight: minHeight }">
-    <q-card-section class="q-pb-none">
-      <div class="row justify-between items-center">
-        <div class="col-10">
-          <span add-edit-form-header class="text-h4 text-weight-medium">
-            {{ isEditing ? 'Edit' : 'Add' }} {{ label }}
-          </span>
-        </div>
-        <div class="col-auto">
-          <q-btn
-            padding="sm"
-            size="xs"
-            rounded
-            icon="close"
-            color="red-5"
-            @click="emits('close')"
-          />
-        </div>
+    <q-card-section class="q-py-none row justify-between items-center">
+      <div class="col-10">
+        <span add-edit-form-header class="text-h4 text-weight-medium">
+          {{ isEditing ? 'Edit ' : 'Add ' }}{{ label }}
+        </span>
+      </div>
+      <div class="col-auto">
+        <q-btn
+          padding="sm"
+          size="xs"
+          rounded
+          icon="close"
+          color="red-5"
+          @click="emits('close')"
+        />
       </div>
     </q-card-section>
     <q-form
@@ -24,10 +22,17 @@
       @submit.prevent="emits('submit')"
       @reset="emits('reset', { ...initialData })"
     >
-      <q-card-section class="col-grow">
+      <q-card-section
+        class="col-grow"
+        :style="{
+          'max-height': `${screenWidth < 600 ? '85vh' : '65vh'}`,
+          'min-height': '200px',
+          'overflow-Y': 'auto',
+        }"
+      >
         <slot />
       </q-card-section>
-      <q-card-actions align="right" class="q-pa-lg">
+      <q-card-actions align="right" class="q-pa-lg absolute-bottom">
         <q-btn
           type="submit"
           v-if="isEditing"
@@ -50,6 +55,8 @@
 </template>
 
 <script setup lang="ts">
+import { useScreenSize } from 'src/composables/utilComposibles';
+
 interface Props {
   label: string;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -66,10 +73,11 @@ const props = withDefaults(defineProps<Props>(), {
 
 const initialData = { ...props.initialObject };
 const emits = defineEmits(['close', 'submit', 'reset']);
+const { screenWidth } = useScreenSize();
 </script>
 
 <style scoped>
 [add-edit-form-header] {
-  font-size: calc(var(--c-font-size) + 1vw);
+  font-size: min(calc(var(--c-font-size) + 2vw), 2rem);
 }
 </style>
